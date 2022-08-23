68c355aca715b05c701445093868356bb1953649
Jaewook Lee <jaewooklee042@gmail.com>
2022-08-23 17:47:22 +0200

    apply format

--- Sources/ParameterSpaces/zero_degree_b_spline_basis_function.cpp
+++ Sources/ParameterSpaces/zero_degree_b_spline_basis_function.cpp
@@ -1,17 +1,21 @@
 /* Copyright (c) 2018–2021 SplineLib
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include "Sources/ParameterSpaces/zero_degree_b_spline_basis_function.hpp"
 
@@ -21,36 +25,48 @@ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 
 namespace splinelib::sources::parameter_spaces {
 
-ZeroDegreeBSplineBasisFunction::ZeroDegreeBSplineBasisFunction(KnotVector const &knot_vector,
-    KnotSpan const &start_of_support, Tolerance const &tolerance) : BSplineBasisFunction(knot_vector, start_of_support,
-                                                                                         Degree{}, tolerance) {}
+ZeroDegreeBSplineBasisFunction::ZeroDegreeBSplineBasisFunction(
+    KnotVector const& knot_vector,
+    KnotSpan const& start_of_support,
+    Tolerance const& tolerance)
+    : BSplineBasisFunction(knot_vector, start_of_support, Degree{}, tolerance) {
+}
 
-bool IsEqual(ZeroDegreeBSplineBasisFunction const &lhs, ZeroDegreeBSplineBasisFunction const &rhs,
-             Tolerance const &tolerance) {
+bool IsEqual(ZeroDegreeBSplineBasisFunction const& lhs,
+             ZeroDegreeBSplineBasisFunction const& rhs,
+             Tolerance const& tolerance) {
   using Base = ZeroDegreeBSplineBasisFunction::Base_;
 
 #ifndef NDEBUG
   try {
     utilities::numeric_operations::ThrowIfToleranceIsNegative(tolerance);
-  } catch (InvalidArgument const &exception) {
-    Throw(exception, "splinelib::sources::parameter_spaces::IsEqual::ZeroDegreeBSplineBasisFunction");
+  } catch (InvalidArgument const& exception) {
+    Throw(exception,
+          "splinelib::sources::parameter_spaces::IsEqual::"
+          "ZeroDegreeBSplineBasisFunction");
   }
 #endif
-  return IsEqual(static_cast<Base const &>(lhs), static_cast<Base const &>(rhs), tolerance);
+  return IsEqual(static_cast<Base const&>(lhs),
+                 static_cast<Base const&>(rhs),
+                 tolerance);
 }
 
-bool operator==(ZeroDegreeBSplineBasisFunction const &lhs, ZeroDegreeBSplineBasisFunction const &rhs) {
+bool operator==(ZeroDegreeBSplineBasisFunction const& lhs,
+                ZeroDegreeBSplineBasisFunction const& rhs) {
   return IsEqual(lhs, rhs);
 }
 
 ZeroDegreeBSplineBasisFunction::Type_
-ZeroDegreeBSplineBasisFunction::operator()(ParametricCoordinate const &parametric_coordinate,
-                                           Tolerance const &tolerance) const {
+ZeroDegreeBSplineBasisFunction::operator()(
+    ParametricCoordinate const& parametric_coordinate,
+    Tolerance const& tolerance) const {
 #ifndef NDEBUG
   try {
     utilities::numeric_operations::ThrowIfToleranceIsNegative(tolerance);
-  } catch (InvalidArgument const &exception) {
-    Throw(exception, "splinelib::sources::parameter_spaces::ZeroDegreeBSplineBasisFunction::operator()");
+  } catch (InvalidArgument const& exception) {
+    Throw(exception,
+          "splinelib::sources::parameter_spaces::"
+          "ZeroDegreeBSplineBasisFunction::operator()");
   }
 #endif
   return (IsInSupport(parametric_coordinate, tolerance) ? Type_{1.0} : Type_{});
@@ -58,43 +74,51 @@ ZeroDegreeBSplineBasisFunction::operator()(ParametricCoordinate const &parametri
 
 ZeroDegreeBSplineBasisFunction::Type_
 ZeroDegreeBSplineBasisFunction::operator()(
-    ParametricCoordinate const &parametric_coordinate,
+    ParametricCoordinate const& parametric_coordinate,
     UniqueEvaluations& unique_evaluations,
-    int const &tree_info,
-    Tolerance const &tolerance) const {
+    int const& tree_info,
+    Tolerance const& tolerance) const {
 
   // At each Spline evaluation, this function will be called exactly 4 times
-  // per dim. So, let's just compute. It is also generally faster 
+  // per dim. So, let's just compute. It is also generally faster
   return operator()(parametric_coordinate, tolerance);
 }
 
 ZeroDegreeBSplineBasisFunction::Type_
-ZeroDegreeBSplineBasisFunction::operator()(ParametricCoordinate const &parametric_coordinate,
-                                           Derivative const &derivative, Tolerance const &tolerance) const {
+ZeroDegreeBSplineBasisFunction::operator()(
+    ParametricCoordinate const& parametric_coordinate,
+    Derivative const& derivative,
+    Tolerance const& tolerance) const {
 #ifndef NDEBUG
   try {
     utilities::numeric_operations::ThrowIfToleranceIsNegative(tolerance);
-  } catch (InvalidArgument const &exception) {
-    Throw(exception, "splinelib::sources::parameter_spaces::ZeroDegreeBSplineBasisFunction::operator()");
+  } catch (InvalidArgument const& exception) {
+    Throw(exception,
+          "splinelib::sources::parameter_spaces::"
+          "ZeroDegreeBSplineBasisFunction::operator()");
   }
 #endif
-  return (derivative == Derivative{} ? operator()(parametric_coordinate, tolerance) : Type_{});
+  return (
+      derivative == Derivative{} ? operator()(parametric_coordinate, tolerance)
+                                 : Type_{});
 }
 
 ZeroDegreeBSplineBasisFunction::Type_
 ZeroDegreeBSplineBasisFunction::operator()(
-    ParametricCoordinate const &parametric_coordinate,
-    Derivative const &derivative,
+    ParametricCoordinate const& parametric_coordinate,
+    Derivative const& derivative,
     UniqueDerivatives& unique_derivatives,
     UniqueEvaluations& unique_evaluations,
     IsTopLevelComputed& top_level_computed,
-    int const &tree_info,
-    Tolerance const &tolerance) const {
+    int const& tree_info,
+    Tolerance const& tolerance) const {
 #ifndef NDEBUG
   try {
     utilities::numeric_operations::ThrowIfToleranceIsNegative(tolerance);
-  } catch (InvalidArgument const &exception) {
-    Throw(exception, "splinelib::sources::parameter_spaces::ZeroDegreeBSplineBasisFunction::operator()");
+  } catch (InvalidArgument const& exception) {
+    Throw(exception,
+          "splinelib::sources::parameter_spaces::"
+          "ZeroDegreeBSplineBasisFunction::operator()");
   }
 #endif
   if (derivative == Derivative{}) {
@@ -107,4 +131,4 @@ ZeroDegreeBSplineBasisFunction::operator()(
   }
 }
 
-}  // namespace splinelib::sources::parameter_spaces
+} // namespace splinelib::sources::parameter_spaces
--- Sources/Utilities/system_operations.hpp
+++ Sources/Utilities/system_operations.hpp
@@ -1,17 +1,21 @@
 /* Copyright (c) 2018–2021 SplineLib
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
-rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
-persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
-Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #ifndef SOURCES_UTILITIES_SYSTEM_OPERATIONS_HPP_
 #define SOURCES_UTILITIES_SYSTEM_OPERATIONS_HPP_
@@ -36,16 +40,17 @@ using LocalTime = std::tm;
 using Mode = std::ios_base::openmode;
 using OutputStream = std::ofstream;
 
-constexpr sources::utilities::system_operations::Mode const kModeAppend{std::ios_base::app}, kModeIn{std::ios_base::in},
-                                                            kModeOut{std::ios_base::out};
+constexpr sources::utilities::system_operations::Mode const kModeAppend{
+    std::ios_base::app},
+    kModeIn{std::ios_base::in}, kModeOut{std::ios_base::out};
 
 LocalTime GetLocalTime();
 
 template<typename FileStream, Mode mode>
-FileStream Open(File const &file);
+FileStream Open(File const& file);
 
 #include "Sources/Utilities/system_operations.inc"
 
-}  // namespace splinelib::sources::utilities::system_operations
+} // namespace splinelib::sources::utilities::system_operations
 
-#endif  // SOURCES_UTILITIES_SYSTEM_OPERATIONS_HPP_
+#endif // SOURCES_UTILITIES_SYSTEM_OPERATIONS_HPP_
