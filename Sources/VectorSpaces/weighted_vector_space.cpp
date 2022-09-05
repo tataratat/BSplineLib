fa8e09def9255b471f43811f71358517cc661eb0
Jaewook Lee <jaewooklee042@gmail.com>
2022-09-05 06:49:27 +0200

    explicit w v space

---
+++ Sources/VectorSpaces/weighted_vector_space.cpp
@@ -0,0 +1,14 @@
+#ifdef SPLINELIB_EXPLICIT_STATIC_LIBRARY
+#include "Sources/VectorSpaces/weighted_vector_space.hpp"
+
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<1>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<2>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<3>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<4>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<5>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<6>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<7>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<8>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<9>;
+template class splinelib::sources::vector_spaces::WeightedVectorSpace<10>;
+#endif
