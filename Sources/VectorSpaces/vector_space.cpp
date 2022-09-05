a22affce6719c98327aedfa7c8fef4159d2a4369
Jaewook Lee <jaewooklee042@gmail.com>
2022-09-05 06:46:35 +0200

    explicit v space

---
+++ Sources/VectorSpaces/vector_space.cpp
@@ -0,0 +1,14 @@
+#ifdef SPLINELIB_EXPLICIT_STATIC_LIBRARY
+#include "Sources/VectorSpaces/vector_space.hpp"
+
+template class splinelib::sources::vector_spaces::VectorSpace<1>;
+template class splinelib::sources::vector_spaces::VectorSpace<2>;
+template class splinelib::sources::vector_spaces::VectorSpace<3>;
+template class splinelib::sources::vector_spaces::VectorSpace<4>;
+template class splinelib::sources::vector_spaces::VectorSpace<5>;
+template class splinelib::sources::vector_spaces::VectorSpace<6>;
+template class splinelib::sources::vector_spaces::VectorSpace<7>;
+template class splinelib::sources::vector_spaces::VectorSpace<8>;
+template class splinelib::sources::vector_spaces::VectorSpace<9>;
+template class splinelib::sources::vector_spaces::VectorSpace<10>;
+#endif
