b00523e8c70a673129150e9637b80d9b6126604a
Jaewook Lee <jaewooklee042@gmail.com>
2022-11-24 07:43:17 +0100

    add spline.cpp

---
+++ Sources/Splines/spline.cpp
@@ -0,0 +1,112 @@
+#ifdef SPLINELIB_EXPLICIT_STATIC_LIBRARY
+#include "Sources/Splines/nurbs.hpp"
+template class splinelib::sources::splines::Spline<1, 1>;
+template class splinelib::sources::splines::Spline<1, 2>;
+template class splinelib::sources::splines::Spline<1, 3>;
+template class splinelib::sources::splines::Spline<1, 4>;
+template class splinelib::sources::splines::Spline<1, 5>;
+template class splinelib::sources::splines::Spline<1, 6>;
+template class splinelib::sources::splines::Spline<1, 7>;
+template class splinelib::sources::splines::Spline<1, 8>;
+template class splinelib::sources::splines::Spline<1, 9>;
+template class splinelib::sources::splines::Spline<1, 10>;
+
+template class splinelib::sources::splines::Spline<2, 1>;
+template class splinelib::sources::splines::Spline<2, 2>;
+template class splinelib::sources::splines::Spline<2, 3>;
+template class splinelib::sources::splines::Spline<2, 4>;
+template class splinelib::sources::splines::Spline<2, 5>;
+template class splinelib::sources::splines::Spline<2, 6>;
+template class splinelib::sources::splines::Spline<2, 7>;
+template class splinelib::sources::splines::Spline<2, 8>;
+template class splinelib::sources::splines::Spline<2, 9>;
+template class splinelib::sources::splines::Spline<2, 10>;
+
+template class splinelib::sources::splines::Spline<3, 1>;
+template class splinelib::sources::splines::Spline<3, 2>;
+template class splinelib::sources::splines::Spline<3, 3>;
+template class splinelib::sources::splines::Spline<3, 4>;
+template class splinelib::sources::splines::Spline<3, 5>;
+template class splinelib::sources::splines::Spline<3, 6>;
+template class splinelib::sources::splines::Spline<3, 7>;
+template class splinelib::sources::splines::Spline<3, 8>;
+template class splinelib::sources::splines::Spline<3, 9>;
+template class splinelib::sources::splines::Spline<3, 10>;
+
+template class splinelib::sources::splines::Spline<4, 1>;
+template class splinelib::sources::splines::Spline<4, 2>;
+template class splinelib::sources::splines::Spline<4, 3>;
+template class splinelib::sources::splines::Spline<4, 4>;
+template class splinelib::sources::splines::Spline<4, 5>;
+template class splinelib::sources::splines::Spline<4, 6>;
+template class splinelib::sources::splines::Spline<4, 7>;
+template class splinelib::sources::splines::Spline<4, 8>;
+template class splinelib::sources::splines::Spline<4, 9>;
+template class splinelib::sources::splines::Spline<4, 10>;
+
+template class splinelib::sources::splines::Spline<5, 1>;
+template class splinelib::sources::splines::Spline<5, 2>;
+template class splinelib::sources::splines::Spline<5, 3>;
+template class splinelib::sources::splines::Spline<5, 4>;
+template class splinelib::sources::splines::Spline<5, 5>;
+template class splinelib::sources::splines::Spline<5, 6>;
+template class splinelib::sources::splines::Spline<5, 7>;
+template class splinelib::sources::splines::Spline<5, 8>;
+template class splinelib::sources::splines::Spline<5, 9>;
+template class splinelib::sources::splines::Spline<5, 10>;
+
+template class splinelib::sources::splines::Spline<6, 1>;
+template class splinelib::sources::splines::Spline<6, 2>;
+template class splinelib::sources::splines::Spline<6, 3>;
+template class splinelib::sources::splines::Spline<6, 4>;
+template class splinelib::sources::splines::Spline<6, 5>;
+template class splinelib::sources::splines::Spline<6, 6>;
+template class splinelib::sources::splines::Spline<6, 7>;
+template class splinelib::sources::splines::Spline<6, 8>;
+template class splinelib::sources::splines::Spline<6, 9>;
+template class splinelib::sources::splines::Spline<6, 10>;
+
+template class splinelib::sources::splines::Spline<7, 1>;
+template class splinelib::sources::splines::Spline<7, 2>;
+template class splinelib::sources::splines::Spline<7, 3>;
+template class splinelib::sources::splines::Spline<7, 4>;
+template class splinelib::sources::splines::Spline<7, 5>;
+template class splinelib::sources::splines::Spline<7, 6>;
+template class splinelib::sources::splines::Spline<7, 7>;
+template class splinelib::sources::splines::Spline<7, 8>;
+template class splinelib::sources::splines::Spline<7, 9>;
+template class splinelib::sources::splines::Spline<7, 10>;
+
+template class splinelib::sources::splines::Spline<8, 1>;
+template class splinelib::sources::splines::Spline<8, 2>;
+template class splinelib::sources::splines::Spline<8, 3>;
+template class splinelib::sources::splines::Spline<8, 4>;
+template class splinelib::sources::splines::Spline<8, 5>;
+template class splinelib::sources::splines::Spline<8, 6>;
+template class splinelib::sources::splines::Spline<8, 7>;
+template class splinelib::sources::splines::Spline<8, 8>;
+template class splinelib::sources::splines::Spline<8, 9>;
+template class splinelib::sources::splines::Spline<8, 10>;
+
+template class splinelib::sources::splines::Spline<9, 1>;
+template class splinelib::sources::splines::Spline<9, 2>;
+template class splinelib::sources::splines::Spline<9, 3>;
+template class splinelib::sources::splines::Spline<9, 4>;
+template class splinelib::sources::splines::Spline<9, 5>;
+template class splinelib::sources::splines::Spline<9, 6>;
+template class splinelib::sources::splines::Spline<9, 7>;
+template class splinelib::sources::splines::Spline<9, 8>;
+template class splinelib::sources::splines::Spline<9, 9>;
+template class splinelib::sources::splines::Spline<9, 10>;
+
+template class splinelib::sources::splines::Spline<10, 1>;
+template class splinelib::sources::splines::Spline<10, 2>;
+template class splinelib::sources::splines::Spline<10, 3>;
+template class splinelib::sources::splines::Spline<10, 4>;
+template class splinelib::sources::splines::Spline<10, 5>;
+template class splinelib::sources::splines::Spline<10, 6>;
+template class splinelib::sources::splines::Spline<10, 7>;
+template class splinelib::sources::splines::Spline<10, 8>;
+template class splinelib::sources::splines::Spline<10, 9>;
+template class splinelib::sources::splines::Spline<10, 10>;
+#endif
