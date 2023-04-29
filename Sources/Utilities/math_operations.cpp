6b1c4476fc43047bbb0e271f53ad15b1942c3264
Jaewook Lee <jaewooklee042@gmail.com>
2023-04-29 09:42:12 +0200

    add RemoveOneParametricDimension

--- Sources/Utilities/math_operations.cpp
+++ Sources/Utilities/math_operations.cpp
@@ -19,7 +19,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include "Sources/Utilities/math_operations.hpp"
 
-//#include <cmath> // NOLINT(whitespace/comments)
+// #include <cmath> // NOLINT(whitespace/comments)
 
 #include "Sources/Utilities/error_handling.hpp"
 
